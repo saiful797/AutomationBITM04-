5cf9fd92dd051d4a7ab95831ca222dddfd6c3ddf
saiful797 <saifulislamsaif797@gmail.com>
2021-12-30 19:43:55 +0600

    Firefox Launch

--- src/main/java/com/WebDriverBasic/Browser.java
+++ src/main/java/com/WebDriverBasic/Browser.java
@@ -2,13 +2,36 @@ package com.WebDriverBasic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 public class Browser {
-    public static void main(String[] args) {
-        WebDriver  driver;
 
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+        chrome_launch();
+        firefox_launch();
+        chrome_close();
+        firefox_close();
+    }
+
+    public static void chrome_launch(){
         System.setProperty("webdriver.chrome.driver", "./src/main/resources/Drivers/chromedriver.exe");
         driver=new ChromeDriver();
+    }
+
+    public static void firefox_launch(){
+        System.setProperty("webdriver.gecko.driver", "./src/main/resources/Drivers/geckodriver.exe");
+        driver=new FirefoxDriver();
+    }
+
+    public static void firefox_close(){
+        driver.close();
+    }
+
+    public static void chrome_close(){
         driver.close();
     }
 }
